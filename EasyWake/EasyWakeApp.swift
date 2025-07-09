a918ed3ebcc40a738fa070f7da31459c64dca34e
jnadimpalli <jnadimpalli@gmail.com>
2025-07-09 15:27:19 -0400

    Initial Commit

---
+++ EasyWake/EasyWakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyWakeApp.swift
+//  EasyWake
+//
+//  Created by Jitesh Nadimpalli on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct EasyWakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EasyWakeTests/EasyWakeTests.swift
@@ -0,0 +1,16 @@
+//
+//  EasyWakeTests.swift
+//  EasyWakeTests
+//
+//  Created by Jitesh Nadimpalli on 7/9/25.
+//
+
+import Testing
+
+struct EasyWakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
